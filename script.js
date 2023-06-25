0a2cfdb8f053513256ded9fd5341c4a4aa954db1
burkewald <70238524+burkewald@users.noreply.github.com>
2023-06-24 21:16:35 -0700

    wip

--- script.js
+++ script.js
@@ -3,22 +3,3 @@
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
   });
-
-
-
-  // 3d mouse
-  const el = document.getElementById('followContainer')
-
-  function detectaCoordsElem(e){
-     var coords = e.getBoundingClientRect()
-     return{ x: coords.left + coords.width/2, y: coords.top + coords.height/2}
-  }
-  function mueveElem(el,x,y){
-     el.style.setProperty('transform',`rotateY(${x/20}deg) rotateX(${-y/20}deg)`)
-  }
-
-  document.addEventListener('mousemove', function(e){
-     var x = e.clientX-detectaCoordsElem(el).x
-     var y = e.clientY-detectaCoordsElem(el).y
-     mueveElem(el,x,y)
-  })
