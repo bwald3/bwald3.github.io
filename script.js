54ebd7bc3f88d9ddb19f6f963d92408cc24bc17f
waldburke <burkewald4@outlook.com>
2024-10-03 21:38:01 -0700

    wip

--- script.js
+++
@@ -1,7 +0,0 @@
-scroll.on("call", (func, direction, obj) => {
-  if (func === "grow" && direction === "enter") {
-    obj.el.classList.add("grow");
-  } else if (func === "grow" && direction === "leave") {
-    obj.el.classList.remove("grow");
-  }
-});
